0f9fea0119b347b2ff31c46639dffc85a4a7f9e4
Abhishekgite <109425308+Abhishekgite@users.noreply.github.com>
2024-01-08 00:18:32 +0530

    We have addded the SoftAssert and created a new Account

--- src/main/java/PageObjectModel/RegisterationPageObject.java
+++ src/main/java/PageObjectModel/RegisterationPageObject.java
@@ -14,28 +14,85 @@ public class RegisterationPageObject {
 
 	private By email = By.xpath("//input[@name='email']");
 
+	private By telephone = By.xpath("//input[@name='telephone']");
+
+	private By password = By.xpath("//input[@name='password']");
+
+	private By passwordconfirm = By.xpath("//input[@name='confirm']");
+
+	private By subscribe = By.xpath("(//input[@name='newsletter'])[2]");
+
+	private By privacyPolicy = By.xpath("//input[@name='agree']");
+
+	private By continueButton = By.xpath("//input[@value='Continue']");
+	
+	private By firstNameErrorMsg = By.xpath("(//div[@class='text-danger'])[1]");
+	
+	private By lastNameErrorMsg = By.xpath("(//div[@class='text-danger'])[2]");
+	
+
+
 	// driver 2 took the scope of driver from register test cases class
 
 	public RegisterationPageObject(WebDriver driver2) {
 
-		// TODO Auto-generated constructor stub
 
 		this.driver = driver2;
 	}
 
-	public WebElement enterFirstname() {
+	public WebElement EnterFirstname() {
 
 		return driver.findElement(firstname);
 	}
 
-	public WebElement enterLastname() {
+	public WebElement EnterLastname() {
 
 		return driver.findElement(lastname);
 	}
 
-	public WebElement enterEmail() {
+	public WebElement EnterEmail() {
 
 		return driver.findElement(email);
 	}
 
+	public WebElement EnterTelephone() {
+
+		return driver.findElement(telephone);
+	}
+
+	public WebElement EnterPassword() {
+
+		return driver.findElement(password);
+	}
+
+	public WebElement EnterPasswordConfirm() {
+
+		return driver.findElement(passwordconfirm);
+	}
+
+	public WebElement ClickOnSubscribe() {
+
+		return driver.findElement(subscribe);
+	}
+
+	public WebElement AcceptPrivacyPolicy() {
+
+		return driver.findElement(privacyPolicy);
+	}
+
+	public WebElement ClickOnContinueButton() {
+
+		return driver.findElement(continueButton);
+	}
+	
+	public WebElement CaptureFirstNameErrorMsg() {
+
+		return driver.findElement(firstNameErrorMsg);
+	}
+	
+	public WebElement CaptureLastNameErrorMsg() {
+
+		return driver.findElement(lastNameErrorMsg);
+	}
 }
+
--- src/test/java/testcases/RegisterTestCases.java
+++ src/test/java/testcases/RegisterTestCases.java
@@ -1,8 +1,13 @@
 package testcases;
 
 import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 import com.Resources.baseClass2;
 
 import PageObjectModel.HomePageObject;
@@ -11,41 +16,63 @@ import PageObjectModel.RegisterationPageObject;
 public class RegisterTestCases extends baseClass2 {
 
 	@Test
-	public void verifyRegisterationWithValidData() throws IOException, InterruptedException {
+	public void verifyRegisterationWithBlankData() throws IOException, InterruptedException {
 
 		Thread.sleep(3000);
-		
+
 		HomePageObject hpo = new HomePageObject(driver);
 		hpo.clickonMyAccount().click();
 		hpo.clickonregister().click();
-		
-		
 
 		RegisterationPageObject rop = new RegisterationPageObject(driver);
-		
-		rop.enterFirstname().clear();
-		
-		rop.enterFirstname().sendKeys("test");
-		rop.enterLastname().clear();
-		rop.enterLastname().sendKeys("test123");
 
-		rop.enterEmail().clear();
-		rop.enterEmail().sendKeys("test@gmail.com");
+		rop.EnterFirstname().clear();
+		rop.EnterFirstname().sendKeys("");
+		rop.EnterLastname().clear();
+		rop.EnterLastname().sendKeys("");
 
-		//driver.findElement(By.xpath("")).sendKeys(null);
+		rop.EnterEmail().clear();
+		rop.EnterEmail().sendKeys("");
+		
+		rop.EnterTelephone().sendKeys("");
+		rop.EnterPassword().sendKeys("");
+		rop.EnterPasswordConfirm().sendKeys("");
+		rop.ClickOnSubscribe().click();
+		rop.AcceptPrivacyPolicy().click();
+		rop.ClickOnContinueButton().click();
+
+		
+		SoftAssert sa = new SoftAssert();
+		
+		String firstNameErrorMsg = "First Name must be between 1 and 32 characters!";
+		
+		String lastNameErrorMsg = "Last Name must be between 1 and 32 characters!";
+		
+		sa.assertEquals(rop.CaptureFirstNameErrorMsg().getText(), firstNameErrorMsg);
+		
+		sa.assertEquals(rop.CaptureLastNameErrorMsg().getText(), lastNameErrorMsg);
+	
+		sa.assertAll();
 	}
-	@Test
+
+	@Test 
 	public void verifyRegisterationWithInValidData() throws IOException, InterruptedException {
+
+		Thread.sleep(3000);
+
+		RegisterationPageObject rop = new RegisterationPageObject(driver);
+		rop.EnterFirstname().sendKeys("test");
+		rop.EnterLastname().sendKeys("test");
+
+		rop.EnterEmail().sendKeys("te123@gmail.com");
 		
-				Thread.sleep(3000);
-
-				RegisterationPageObject rop = new RegisterationPageObject(driver);
-				rop.enterFirstname().sendKeys("test");
-				rop.enterLastname().sendKeys("test");
-
-				rop.enterEmail().sendKeys("testgmail.com");
-
 		
+		rop.EnterTelephone().sendKeys("12345678");
+		rop.EnterPassword().sendKeys("test123");
+		rop.EnterPasswordConfirm().sendKeys("test123");
+		rop.ClickOnSubscribe().click();
+		rop.ClickOnContinueButton().click();
+
 	}
 
 }
